05a07337e89d52f7e76b8c7720143050795ccb82
Arrebol <jiazengp@gmail.com>
2023-08-14 11:42:06 +0800

    New translations zh.ts (French) [skip ci] Update translations (French) zh.ts

--- .vitepress/locales/fr.ts
+++ .vitepress/locales/fr.ts
@@ -1,380 +1,320 @@
-import fs from 'fs'
-import path from 'path'
-import { baseHelper } from '../theme/utils'
-
-import type { DefaultTheme, LocaleSpecificConfig } from 'vitepress'
-import type { CustomConfig } from './types'
-
-export const META_URL = 'https://yuanshen.site/docs/fr/'
-export const META_TITLE = 'Genshin Interactive Map'
-export const META_KEYWORDS =
-  'Genshin Interactive Map, Genshin Map, Kongying Tavern, yuanshenmap, Genshin Impact Map, Kongying Map'
-export const META_DESCRIPTION =
-  'A Genshin interactive map by Kongying Tavern for completionists'
-export const META_IMAGE = 'https://yuanshen.site/docs/imgs/cover.jpg'
-export const LOCAL_CODE = 'fr'
-export const LOCAL_BASE = 'fr'
-
-export const frConfig: LocaleSpecificConfig<
-  DefaultTheme.Config & CustomConfig
-> = {
+import { baseHelper } from '../theme/utils';
+import { socialList } from '../theme/composables/socialList';
+import type { DefaultTheme, LocaleSpecificConfig } from 'vitepress';
+import type { CustomConfig } from './types';
+export const META_URL = 'https://yuanshen.site/docs/';
+export const META_TITLE = "Genshin Interactive Map";
+export const META_DESCRIPTION = "A Genshin interactive map by Kongying Tavern for completionists";
+export const META_KEYWORDS = "Genshin Interactive Map, Genshin Map, Kongying Tavern, yuanshenmap, Genshin Impact Map, Kongying Map";
+export const META_IMAGE = 'https://yuanshen.site/docs/imgs/cover.jpg';
+export const LOCAL_CODE = "fr";
+export const LOCAL_BASE = '';
+export const zhConfig: LocaleSpecificConfig<DefaultTheme.Config & CustomConfig> = {
   description: META_DESCRIPTION,
-  titleTemplate: 'Kongying Tavern',
+  titleTemplate: '空荧酒馆',
+  head: [['meta', {
+    name: 'keywords',
+    content: META_KEYWORDS
+  }], ['meta', {
+    property: 'og:url',
+    content: META_URL
+  }], ['meta', {
+    property: 'og:description',
+    content: META_DESCRIPTION
+  }], ['meta', {
+    property: 'twitter:url',
+    content: META_URL
+  }], ['meta', {
+    property: 'twitter:title',
+    content: META_TITLE
+  }], ['meta', {
+    property: 'og:site_name',
+    content: META_TITLE
+  }], ['meta', {
+    property: 'twitter:description',
+    content: META_DESCRIPTION
+  }], ['meta', {
+    property: 'og:locale',
+    content: LOCAL_CODE
+  }], ['meta', {
+    property: 'og:image',
+    content: META_IMAGE
+  }], ['meta', {
+    property: 'og:image',
+    content: META_IMAGE
+  }], ['meta', {
+    name: 'twitter:image',
+    content: META_IMAGE
+  }]],
   themeConfig: {
-    siteTitle: 'Carte interactive de Genshin',
-    outlineTitle: 'Sur cette page',
+    siteTitle: '原神地图',
+    outlineTitle: '本页目录',
     logo: '/imgs/logo_256.png',
-    lastUpdatedText: 'Dernière mise à jour ',
+    lastUpdatedText: '更新日期',
+    returnToTopLabel: '回到顶部',
+    langMenuLabel: '更改语言',
     notFound: {
-      title: 'PAGE NOT FOUNDz',
-      quote:
-        "But if you don't change your direction, and if you keep looking, you may end up where you are heading.",
-      linkLabel: 'Take me home',
+      title: '这个页面找不到了',
+      quote: '可能跟温迪一起出游了吧',
+      linkLabel: '回到首页'
     },
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/kongying-tavern' },
-      {
-        icon: {
-          svg: fs.readFileSync(
-            path.resolve(__dirname, '../../src/public/svg/reddit.svg'),
-            'utf8',
-          ),
-        },
-        link: 'https://www.reddit.com/user/Kongying_Tavern',
+    socialLinks: [{
+      icon: 'github',
+      link: 'https://github.com/kongying-tavern/'
+    }, {
+      icon: 'discord',
+      link: 'https://discord.gg/aFe57AKZUF'
+    }, {
+      icon: 'twitter',
+      link: 'https://twitter.com/KongyingTavern'
+    }, {
+      icon: {
+        svg: socialList.bilibili.icon
       },
-      { icon: 'discord', link: 'https://discord.gg/aFe57AKZUF' },
-      { icon: 'twitter', link: 'https://twitter.com/KongyingTavern' },
-    ],
+      link: 'https://space.bilibili.com/518076785',
+      ariaLabel: 'bilibili'
+    }],
     docFooter: {
-      prev: 'Page précédente',
-      next: 'Page suivante',
-    },
-    editLink: {
-      pattern: 'https://github.com/kongying-tavern/docs/edit/next/src/:path',
-      text: 'Suggérer des modifications à cette page',
+      prev: '上一页',
+      next: '下一页'
     },
     payment: {
       wechatpay: {
-        name: 'WeChat Pay',
-        address: 'wxp://f2f0dd1rszrnqJc_gnlwV_lRX5dlZ1Dtn9rp',
+        name: '微信支付',
+        address: 'wxp://f2f0dd1rszrnqJc_gnlwV_lRX5dlZ1Dtn9rp'
       },
       alipay: {
-        name: 'Alipay',
-        address: 'https://qr.alipay.com/tsx11609thmpw9odmvdlxd6',
+        name: '支付宝',
+        address: 'https://qr.alipay.com/tsx11609thmpw9odmvdlxd6'
       },
       qqpay: {
-        name: 'QQ Pay',
-        address:
-          'https://i.qianbao.qq.com/wallet/sqrcode.htm?m=tenpay&a=1&u=790489566&ac=CAEQ3tP3-AIY0v2k_AU%3D_xxx_sign&n=AAAAAAAA&f=wallet',
+        name: 'QQ 支付',
+        address: 'https://i.qianbao.qq.com/wallet/sqrcode.htm?m=tenpay&a=1&u=790489566&ac=CAEQ3tP3-AIY0v2k_AU%3D_xxx_sign&n=AAAAAAAA&f=wallet'
       },
       paypal: {
         name: 'Paypal',
-        address: 'https://www.paypal.com/paypalme/yuanshenditu',
+        address: 'https://www.paypal.com/paypalme/yuanshenditu'
       },
       bilibili: {
         name: 'bilibili',
-        address: 'https://space.bilibili.com/518076785',
-      },
+        address: 'https://space.bilibili.com/518076785'
+      }
     },
-    nav: baseHelper(nav(), LOCAL_BASE),
+    editLink: {
+      pattern: 'https://github.com/kongying-tavern/docs/edit/next/src/:path',
+      text: '报告错误'
+    },
     sidebar: baseHelper(sidebar(), LOCAL_BASE),
     footer: baseHelper(footer(), LOCAL_BASE),
-  },
-  head: [
-    ['meta', { name: 'keywords', content: META_KEYWORDS }],
-    ['meta', { property: 'og:url', content: META_URL }],
-    ['meta', { property: 'og:description', content: META_DESCRIPTION }],
-    ['meta', { property: 'twitter:url', content: META_URL }],
-    ['meta', { property: 'twitter:title', content: META_TITLE }],
-    ['meta', { property: 'twitter:description', content: META_DESCRIPTION }],
-    ['meta', { property: 'og:site_name', content: META_TITLE }],
-    ['meta', { property: 'og:locale', content: LOCAL_CODE }],
-    ['meta', { property: 'og:image', content: META_IMAGE }],
-    [
-      'meta',
-      {
-        name: 'twitter:image',
-        content: META_IMAGE,
-      },
-    ],
-  ],
-}
+    nav: baseHelper(nav(), LOCAL_BASE)
+  }
+};
 
 function nav(): DefaultTheme.NavItem[] {
-  return [
-    {
-      text: 'Télécharger',
-      link: '/download-client.md',
-    },
-    {
-      text: 'Soutenez-nous',
-      link: '/support-us.md',
-    },
-    {
-      text: 'Communauté',
-      link: '/community.md',
-    },
-    {
-      text: 'Support',
-      items: [
-        {
-          text: "Manuel d'utilisation du client",
-          link: '/manual/client-user-manual.md',
-        },
-        {
-          text: "Retour d'expérience",
-          link: 'https://support.qq.com/products/321980',
-        },
-        {
-          text: 'Nouvelles fonctionnalités',
-          link: 'https://support.qq.com/products/321980/topic-detail/2016/',
-        },
-      ],
-    },
-    {
-      text: 'À propos',
-      items: [
-        {
-          text: 'À propos de nous',
-          items: [
-            {
-              text: 'Rejoignez-nous',
-              link: '/join.md',
-            },
-            {
-              text: 'Notre équipe',
-              link: '/team.md',
-            },
-          ],
-        },
-        {
-          text: 'Journal des mises à jour',
-          items: [
-            {
-              text: 'Web',
-              link: 'https://support.qq.com/products/321980/blog/505810',
-            },
-            {
-              text: 'Client',
-              link: 'https://discord.gg/SWz6RTWNkm',
-            },
-          ],
-        },
-        {
-          text: 'Remerciements',
-          items: [
-            {
-              text: 'Crédits',
-              link: '/credits.md',
-            },
-            {
-              text: 'Contributeurs',
-              link: '/contribution.md',
-            },
-            {
-              text: 'Sponsors',
-              link: '/support-us.md#Sponsor%20Acknowledgements',
-            },
-          ],
-        },
-        {
-          text: 'Mentions légales (chinois)',
-          items: [
-            {
-              text: 'Clause de non-responsabilité',
-              link: '/disclaimer.md',
-            },
-            {
-              text: 'Confidentialité',
-              link: '/privacy.md',
-            },
-            {
-              text: 'Accord',
-              link: '/agreement.md',
-            },
-          ],
-        },
-      ],
-    },
-  ]
+  return [{
+    text: "Télécharger",
+    link: '/download-client.md'
+  }, {
+    text: "Soutenez-nous",
+    link: "/support-us.md"
+  }, {
+    text: "Communauté",
+    link: "/community.md"
+  }, {
+    text: "Support",
+    items: [{
+      text: "Manuel d'utilisation du client",
+      link: 'manual/client-user-manual'
+    }, {
+      text: "Retour d'expérience",
+      link: 'https://support.qq.com/products/321980'
+    }, {
+      text: "Nouvelles fonctionnalités",
+      link: "https://support.qq.com/products/321980/topic-detail/2016/"
+    }, {
+      text: '功能投票',
+      link: 'https://support.qq.com/products/321980/topic-detail/2016/'
+    }]
+  }, {
+    text: "À propos",
+    items: [{
+      text: "À propos de nous",
+      items: [{
+        text: "Rejoignez-nous",
+        link: '/join.md'
+      }, {
+        text: "Notre équipe",
+        link: '/team.md'
+      }]
+    }, {
+      text: "Journal des mises à jour",
+      items: [{
+        text: "Web",
+        link: 'https://support.qq.com/products/321980/blog/505810'
+      }, {
+        text: "Client",
+        link: "https://discord.gg/SWz6RTWNkm"
+      }]
+    }, {
+      text: "Remerciements",
+      items: [{
+        text: "Crédits",
+        link: '/credits.md'
+      }, {
+        text: "Contributeurs",
+        link: '/contribution.md'
+      }, {
+        text: "Sponsors",
+        link: "/support-us.md#Sponsor%20Acknowledgements"
+      }]
+    }, {
+      text: "Mentions légales (chinois)",
+      items: [{
+        text: "Clause de non-responsabilité",
+        link: '/disclaimer.md'
+      }, {
+        text: "Confidentialité",
+        link: '/privacy.md'
+      }, {
+        text: "Accord",
+        link: '/agreement.md'
+      }]
+    }, {
+      text: '其他',
+      items: [{
+        text: '友情链接',
+        link: '/friends-links.md'
+      }]
+    }]
+  }];
 }
 
 function footer(): CustomConfig['footer'] {
   return {
-    qrcodeTitle: 'Serveur Discord',
-    qrcodeMessage: 'Contactez-nous sur Discord',
-    qrcodeLink: 'https://discord.gg/aFe57AKZUF',
-    navigation: [
-      {
-        title: 'À propos',
-        items: [
-          {
-            text: 'Rejoignez-nous',
-            link: '/join',
-          },
-          {
-            text: 'Notre équipe',
-            link: '/team',
-          },
-          {
-            text: 'Sponsors',
-            link: '/support-us',
-          },
-        ],
-      },
-      {
-        title: 'Mentions légales (chinois)',
-        items: [
-          {
-            text: 'Clause de non-responsabilité',
-            link: '/disclaimer',
-          },
-          {
-            text: 'Confidentialité',
-            link: '/privacy',
-          },
-          {
-            text: 'Accord',
-            link: '/agreement',
-          },
-        ],
-      },
-      {
-        title: 'Support',
-        items: [
-          {
-            text: "Manuel d'utilisation du client",
-            link: '/manual/client-user-manual',
-          },
-          {
-            text: "Retour d'expérience",
-            link: 'https://support.qq.com/products/321980',
-          },
-          {
-            text: 'Nouvelles fonctionnalités',
-            link: 'https://support.qq.com/products/321980/topic-detail/2016/',
-          },
-        ],
-      },
-    ],
-  }
+    qrcodeTitle: "Serveur Discord",
+    qrcodeMessage: "Contactez-nous sur Discord",
+    qrcodeLink: "https://discord.gg/aFe57AKZUF",
+    navigation: [{
+      title: "À propos",
+      items: [{
+        text: "Rejoignez-nous",
+        link: '/join'
+      }, {
+        text: "Notre équipe",
+        link: "/team"
+      }, {
+        text: "Sponsors",
+        link: "/support-us"
+      }]
+    }, {
+      title: "Mentions légales (chinois)",
+      items: [{
+        text: "Clause de non-responsabilité",
+        link: '/disclaimer'
+      }, {
+        text: "Confidentialité",
+        link: '/privacy'
+      }, {
+        text: "Accord",
+        link: '/agreement'
+      }]
+    }, {
+      title: "Support",
+      items: [{
+        text: "Manuel d'utilisation du client",
+        link: 'manual/client-user-manual'
+      }, {
+        text: "Retour d'expérience",
+        link: "https://support.qq.com/products/321980"
+      }, {
+        text: "Nouvelles fonctionnalités",
+        link: "https://support.qq.com/products/321980/topic-detail/2016/"
+      }]
+    }]
+  };
 }
 
 function sidebar(): DefaultTheme.SidebarItem[] {
   return {
     // @ts-ignore
-    '/manual': [
-      {
-        text: 'Table des matières',
-        link: '/manual/client-user-manual',
-      },
-      {
-        text: 'Guides',
-        items: [
-          {
-            text: 'Suivi automatique',
-            items: [
-              {
-                text: 'Introduction',
-                link: '/manual/guide/auto-tracking/importantnotes',
-              },
-              {
-                text: 'Résolution des problèmes',
-                link: '/manual/guide/auto-tracking/troubleshoot',
-              },
-            ],
-          },
-          {
-            text: "Contrôle de la fréquence d'images/du suivi",
-            link: '/manual/guide/bg/bgfrate',
-          },
-          {
-            text: 'Marquage multiple',
-            link: '/manual/guide/batch-selection/instructions',
-          },
-          {
-            text: 'Guide du canvas',
-            link: '/manual/guide/canvas/guide',
-          },
-          {
-            text: 'Easter Egg',
-            link: '/manual/guide/easter-egg/view',
-          },
-          {
-            text: 'Cacher/Afficher les éléments trouvés',
-            link: '/manual/guide/hide-show-done/hidedoneshowdone',
-          },
-          {
-            text: 'Mode superposition',
-            items: [
-              {
-                text: 'Instructions',
-                link: '/manual/guide/overlay-mode/instructions',
-              },
-              {
-                text: 'Fenêtré plein écran',
-                link: '/manual/guide/overlay-mode/fullscreen-windowed/launching',
-              },
-            ],
-          },
-          {
-            text: 'Restauration/récupération de la progression',
-            link: '/manual/guide/restore-recover/progress',
-          },
-        ],
-      },
-      {
-        text: 'FAQ',
-        items: [
-          {
-            text: 'Sécurité du compte',
-            link: '/manual/faq/accountsafety/acntban.md',
-          },
-          {
-            text: 'Mise à jour automatique',
-            link: '/manual/faq/autoupdate/updater.md',
-          },
-          {
-            text: "Erreur d'installation",
-            items: [
-              {
-                text: 'Code 2503',
-                link: '/manual/faq/instlerror/code2503.md',
-              },
-              {
-                text: 'Fichier .dll manquant',
-                link: '/manual/faq/instlerror/missingdll.md',
-              },
-            ],
-          },
-          {
-            text: 'Erreur de lancement',
-            items: [
-              {
-                text: 'Blocage/pas de progression',
-                link: '/manual/faq/launcherror/emptydialog.md',
-              },
-              {
-                text: 'Échec de la vérification de la version',
-                link: '/manual/faq/launcherror/versioncheck.md',
-              },
-            ],
-          },
-          {
-            text: 'À propos de la connexion',
-            items: [
-              {
-                text: 'Problèmes de connexion',
-                link: '/manual/faq/login/accountlogin.md',
-              },
-              {
-                text: 'Demandes de connexion répétées',
-                link: '/manual/faq/login/clientrepeatedly.md',
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  }
+    '/manual': [{
+      text: "Table des matières",
+      link: "/manual/client-user-manual"
+    }, {
+      text: "Guides",
+      items: [{
+        text: "Suivi automatique",
+        items: [{
+          text: "Introduction",
+          link: "/manual/guide/auto-tracking/importantnotes"
+        }, {
+          text: "Résolution des problèmes",
+          link: "/manual/guide/auto-tracking/troubleshoot"
+        }]
+      }, {
+        text: "Contrôle de la fréquence d'images/du suivi",
+        link: "/manual/guide/bg/bgfrate"
+      }, {
+        text: "Marquage multiple",
+        link: "/manual/guide/batch-selection/instructions"
+      }, {
+        text: "Guide du canvas",
+        link: "/manual/guide/canvas/guide"
+      }, {
+        text: "Easter Egg",
+        link: "/manual/guide/easter-egg/view"
+      }, {
+        text: "Cacher/Afficher les éléments trouvés",
+        link: "/manual/guide/hide-show-done/hidedoneshowdone"
+      }, {
+        text: "Mode superposition",
+        items: [{
+          text: "Instructions",
+          link: "/manual/guide/overlay-mode/instructions"
+        }, {
+          text: "Fenêtré plein écran",
+          link: "/manual/guide/overlay-mode/fullscreen-windowed/launching"
+        }]
+      }, {
+        text: "Restauration/récupération de la progression",
+        link: "/manual/guide/restore-recover/progress"
+      }]
+    }, {
+      text: "FAQ",
+      items: [{
+        text: "Sécurité du compte",
+        link: "/manual/faq/accountsafety/acntban.md"
+      }, {
+        text: "Mise à jour automatique",
+        link: "/manual/faq/autoupdate/updater.md"
+      }, {
+        text: "Erreur d'installation",
+        items: [{
+          text: "Code 2503",
+          link: "/manual/faq/instlerror/code2503.md"
+        }, {
+          text: "Fichier .dll manquant",
+          link: "/manual/faq/instlerror/missingdll.md"
+        }]
+      }, {
+        text: "Erreur de lancement",
+        items: [{
+          text: "Blocage/pas de progression",
+          link: "/manual/faq/launcherror/emptydialog.md"
+        }, {
+          text: "Échec de la vérification de la version",
+          link: "/manual/faq/launcherror/versioncheck.md"
+        }]
+      }, {
+        text: "À propos de la connexion",
+        items: [{
+          text: "Problèmes de connexion",
+          link: "/manual/faq/login/accountlogin.md"
+        }, {
+          text: "Demandes de connexion répétées",
+          link: "/manual/faq/login/clientrepeatedly.md"
+        }]
+      }]
+    }]
+  };
 }
