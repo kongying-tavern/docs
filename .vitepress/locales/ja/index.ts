5597ca05c1325cf1b532ff3565e73403a3e4ba87
Alex Fang <deeno@qq.com>
2024-04-29 12:44:04 +0800

    refactor: split team in locales

--- .vitepress/locales/ja/index.ts
+++ .vitepress/locales/ja/index.ts
@@ -13,6 +13,7 @@ import UI from './ui'
 import AsideLinks from './aside-links'
 import DocsFeedback from './docs-feedback'
 import Staff from './staff'
+import Team from './team'
 
 export const jaConfig: LocaleSpecificConfig<
   DefaultTheme.Config & CustomConfig
@@ -35,22 +36,7 @@ export const jaConfig: LocaleSpecificConfig<
       next: '次へ',
     },
     staff: Staff,
-    team: {
-      title: '关于团队',
-      desc: '地图的背后是一个基本来自中国的团队，以下是部分成员的个人信息。',
-      coreMember: {
-        title: '核心团队成员',
-        desc: '核心团队成员是那些积极长期参与维护一个或多个核心项目的人。 他们对空荧酒馆的生态系统做出了重大贡献。',
-      },
-      emeritiMember: {
-        title: '名誉核心团队',
-        desc: '我们在此致敬过去曾做出过突出贡献的不再活跃的团队成员。',
-      },
-      partnerMember: {
-        title: '社区伙伴',
-        desc: '我们与这些主要合作伙伴建立了更加亲密的关系，经常与他们就即将到来的功能展开合作。',
-      },
-    },
+    team: Team,
     socialLinks: [
       { icon: 'github', link: 'https://github.com/kongying-tavern' },
       {
