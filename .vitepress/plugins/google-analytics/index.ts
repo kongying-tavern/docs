3882806a99b5e41618daae9b44963f76fabda500
Hyouka <jiazengp@gmail.com>
2025-01-01 16:07:51 +0800

    fix: update

--- .vitepress/plugins/google-analytics/index.ts
+++ .vitepress/plugins/google-analytics/index.ts
@@ -34,7 +34,7 @@ const mountGoogleAnalytics = (id: string, debug: boolean) => {
   // insert gtag snippet
   window.dataLayer = window.dataLayer || []
   // the gtag function must use `arguments` object to forward parameters
-  window.gtag = () => {
+  window.gtag = function () {
     dataLayer.push(arguments)
   }
   gtag('js', new Date())
