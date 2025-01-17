8d36eb9c0020015f366ef7cb3f7dc3a376c8d68f
Hyouka <jiazengp@gmail.com>
2025-01-17 23:39:41 +0800

    fix(forum): 优化话题数据跳转时的缓存处理

--- src/composables/sharedTopicInfo.ts
+++ src/composables/sharedTopicInfo.ts
@@ -5,14 +5,14 @@ export const SHARED_TOPIC_INFO_KEY = 'topic-info'
 
 export const useSharedTopicInfo = (topic?: ForumAPI.Topic) => {
   if (import.meta.env.SSR) return computed(() => null)
+  const data = sessionStorage.getItem(SHARED_TOPIC_INFO_KEY)
 
-  return computed<ForumAPI.Topic>(() => {
-    const data = sessionStorage.getItem(SHARED_TOPIC_INFO_KEY)
-    if (data) {
-      sessionStorage.removeItem(SHARED_TOPIC_INFO_KEY)
-      return JSON.parse(data)
-    }
-    sessionStorage.setItem(SHARED_TOPIC_INFO_KEY, JSON.stringify(topic))
-    return topic
-  })
+  if (data && topic === undefined) {
+    sessionStorage.removeItem(SHARED_TOPIC_INFO_KEY)
+    return computed(() => JSON.parse(data))
+  }
+
+  if (!topic) return computed(() => null)
+  sessionStorage.setItem(SHARED_TOPIC_INFO_KEY, JSON.stringify(topic))
+  return computed<ForumAPI.Topic>(() => topic)
 }
