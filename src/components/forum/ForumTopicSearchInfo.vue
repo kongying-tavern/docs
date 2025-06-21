c3ff48423f4a7bab85185fe0d0be0b60cbd800a5
Hyouka <jiazengp@gmail.com>
2025-06-21 20:09:25 +0800

    fix(forum): 修复退出搜索状态时未清除URL的搜索参数的问题

--- src/components/forum/ForumTopicSearchInfo.vue
+++ src/components/forum/ForumTopicSearchInfo.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
 import { useLocalized } from '@/hooks/useLocalized'
+import { useUrlSearchParams } from '@vueuse/core'
 import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
 import { useForumData } from '~/stores/useForumData'
 
 const forumData = useForumData()
+const params = computed(() => useUrlSearchParams('history', {
+  removeFalsyValues: true,
+  removeNullishValues: true,
+}))
 const { isSearching, topics } = storeToRefs(forumData)
 
 const { message } = useLocalized()
+
 function handleUndo() {
   forumData.refreshData()
+  params.value.q = ''
   isSearching.value = false
 }
 </script>
@@ -17,6 +25,8 @@ function handleUndo() {
 <template>
   <div v-if="isSearching && !forumData.loading" class="w-fit flex items-center rounded-full pl-4 font-size-3.5 hover:bg-[--vp-c-bg-soft]">
     <p class="mr-2 color-[var(--vp-c-text-3)]">
+      {{ message.forum.header.search.placeholder }}
+      ⌈{{ params.q }}⌋ -
       {{ message.forum.header.search.allRelatedContentCount }}
     </p>
     <span>{{ topics.length }}</span>
