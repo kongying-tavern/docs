749e939b5b46db75f30f7564b10823f202b0c797
Hyouka <jiazengp@gmail.com>
2026-08-13 16:27:45 +0800

    refactor: keep ForumUser component

---
+++ src/components/forum/ForumUser.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type ForumAPI from '@/apis/forum/api'
+
+import Avatar from '@/components/ui/Avatar.vue'
+import Button from '@/components/ui/button/Button.vue'
+
+const { user } = defineProps<{
+  user: ForumAPI.User
+}>()
+
+function handleClick() {
+  window.open(`https://gitee.com/${user.login}`)
+}
+</script>
+
+<template>
+  <Button variant="ghost" @click="handleClick">
+    <Avatar size="sm" :src="user.avatar" />
+    <div class="text-left w-fit">
+      <span
+        class="c-[var(--vp-c-text-1)] lh-4 font-[var(--vp-font-family-subtitle)] font-medium block"
+      >
+        {{ user.username }}
+      </span>
+      <span class="c-[var(--vp-c-text-3)] font-[var(--vp-font-family-content)]">
+        @{{ user.login }}
+      </span>
+    </div>
+  </Button>
+</template>
